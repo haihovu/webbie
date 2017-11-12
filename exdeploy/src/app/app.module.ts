42fb717aeb0b7e348e7a96fd58a8ce4c81b05576
Hai Vu <haihovu@gmail.com>
2017-11-12 11:21:57 -0500

    Added more components

--- exdeploy/src/app/app.module.ts
+++ exdeploy/src/app/app.module.ts
@@ -6,9 +6,13 @@ import {ExdeploymentService} from './exdeployment.service';
 import {FormsModule} from '@angular/forms';
 import {HttpModule, XSRFStrategy, CookieXSRFStrategy} from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
+import { PanelComponent } from './panel/panel.component';
+import { FieldComponent } from './field/field.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PanelComponent,
+    FieldComponent
   ],
   imports: [
     BrowserModule,
